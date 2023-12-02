358fb756424f671fa3fff755c0504d74b4f11987
Sreyleak123 <nhorksreyleak@gmail.com>
2023-12-02 09:58:44 +0700

    first commit

---
+++ test1/Controllers/ContacController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Content
+{
+    public class ContacController : Controller
+    {
+        // GET: Contac
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Mycontac()
+        {
+            return View();
+        }
+        public int add()
+        {
+            int a = 4;
+            int b = 5;
+            int s=a+b;
+            return s;
+        }
+
+    }
+}
---
+++ test1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace test1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        // Same name
+        public ActionResult Index()
+        {
+            ViewBag.name = "Show you home Page";
+            return View();
+        }
+        public ActionResult show()
+        {
+            return View();
+        }
+        // Different name
+        public ActionResult showInfo()
+        {
+            return View("show");
+        }
+        // In share Folder
+        public ActionResult Detail()
+        {
+            return View("Details");
+        }
+        // different control
+        public ActionResult showMoreDetail()
+        {
+            return View("~/views/about/Index.cshtml");
+        }
+
+    }
+}
