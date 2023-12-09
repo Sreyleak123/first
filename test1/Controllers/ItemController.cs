accc8fbe7dfa81503b62cc891165911b368752b9
Sreyleak123 <nhorksreyleak@gmail.com>
2023-12-09 15:17:25 +0700

    first commit

--- test1/Controllers/ItemController.cs
+++ test1/Controllers/ItemController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test1.Models;
 
 namespace test1.Controllers
 {
@@ -11,7 +12,18 @@ namespace test1.Controllers
         // GET: Item
         public ActionResult Index()
         {
-            return View();
+
+            ViewBag.shopName = "Computer shop";
+            List<Itemlist> itemlist = new List<Itemlist>() {
+                new Itemlist{Id="001", Name="Acer",price=500 ,Category="Laptop"},
+                new Itemlist{Id="002", Name="Mac",price=1500 ,Category="Laptop"},
+                new Itemlist{Id="003", Name="Asus",price=600 ,Category="Laptop"},
+                new Itemlist{Id="004", Name="dell",price=580 ,Category="Laptop"}
+
+            };
+            itemlist.Add(new Itemlist { Id = "005", Name = "MSI", price = 900, Category = "Laptop" });
+
+            return View(itemlist);
         }
     }
 }
